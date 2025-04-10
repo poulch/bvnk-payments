32185a49b997890b55fc93c7e1575b8ea7e2d535
Paweł Chyła <chyla1988@gmail.com>
2025-04-10 22:18:43 +0200

    Init pay page

--- src/app/payin/[uuid]/pay/page.tsx
+++ src/app/payin/[uuid]/pay/page.tsx
@@ -1,3 +1,55 @@
+import { Card } from "@/components/Card";
+import { Container } from "@/components/Container";
+import { PaymentTitle } from "@/components/PaymentTitle";
+import { Table, TableBody, TableCell, TableRow } from "@/components/Table";
+import { Text } from "@/components/Text";
+
 export default function Pay() {
-  return <div>Pay</div>;
+  return (
+    <Container>
+      <Card>
+        <PaymentTitle>Pay with Bitcoin</PaymentTitle>
+        <p className="text-center">
+          <Text>
+            To complete this payment send the amount due to the BTC address
+            provided below.
+          </Text>
+        </p>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Text>Amount due</Text>
+              </TableCell>
+              <TableCell className="text-right">
+                <Text>0.00410775 BTC COPY</Text>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Text>BTC address</Text>
+              </TableCell>
+              <TableCell className="text-right">
+                <Text>rh6X8bZ...haAdy</Text>
+                COPY
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        Some image here
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Text>Time left to pay</Text>
+              </TableCell>
+              <TableCell className="text-right">
+                <Text>02:59:59</Text>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Card>
+    </Container>
+  );
 }
