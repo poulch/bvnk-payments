e5d69d36d1eb760e5213bbd85e63f38fec8d92d0
Paweł Chyła <chyla1988@gmail.com>
2025-04-10 22:34:16 +0200

    Payment expired page

--- src/app/payin/[uuid]/expired/page.tsx
+++ src/app/payin/[uuid]/expired/page.tsx
@@ -1,3 +1,23 @@
+import { Card } from "@/components/Card";
+import { Container } from "@/components/Container";
+import { PaymentTitle } from "@/components/PaymentTitle";
+import { Text } from "@/components/Text";
+import Image from "next/image";
+
 export default function Expired() {
-  return <div>Expired</div>;
+  return (
+    <Container>
+      <Card className="gap-6 place-content-center h-80 text-center w-full">
+        <Image
+          width="49"
+          height="49"
+          className="mx-auto"
+          src="/warning.svg"
+          alt="Expired"
+        />
+        <PaymentTitle>Payment details expired</PaymentTitle>
+        <Text>The payment details for your transaction  have expired.</Text>
+      </Card>
+    </Container>
+  );
 }
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 export function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-full max-w-md">{children}</div>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <div className="w-full max-w-[460px]">{children}</div>
+    </main>
   );
 }
