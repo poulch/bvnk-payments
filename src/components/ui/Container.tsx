7e0316f7e7de79ecb7b5820f2058468dcfeb5553
Paweł Chyła <chyla1988@gmail.com>
2025-04-14 01:22:23 +0200

    Improve rwd

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,7 +1,7 @@
 export function Container({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex flex-col items-center min-h-screen">
-      <div className="mt-32 w-full max-w-[460px]">{children}</div>
+      <div className="md:mt-32 w-full  md:max-w-[460px]">{children}</div>
     </main>
   );
 }
